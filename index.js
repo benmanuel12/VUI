f751db7238e44ceebd5573d7b3f1f1476ca60f53
Benjamin Manuel <contact.ben.manuel@gmail.com>
2021-04-01 18:01:00 +0100

    edited index

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express();
 let shell = require('shelljs');
+const PORT = process.env.PORT || 5000;
 
 
 app.use(express.static('public'));
 
-app.listen(8080);
+app.listen(PORT);
 console.log("Listening on port 8080")
